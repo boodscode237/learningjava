85fa1c11826246fc0a0cfea0e8750ce94fcf614e
boodscode237 <boodscode@gmail.com>
2020-10-02 16:16:14 +0500

    learned about classses in java

--- src/Maths.java
+++ src/Maths.java
@@ -1,6 +1,30 @@
 public class Maths {
     public static void main(String[] args){
-        double result = Math.pow(2,5);
-        System.out.println(result);
+        double base;
+        double height;
+        double sidLine1;
+        double sideLine2;
+        double sideLine3;
+        Triangle triangleA = new Triangle(
+                base= 15,
+                height = 8,
+                sidLine1= 15,
+                sideLine2= 8,
+                sideLine3= 15 );
+        Triangle triangleB = new Triangle(
+                base= 5,
+                height = 8,
+                sidLine1= 5,
+                sideLine2= 8,
+                sideLine3= 25 );
+
+        double TriangleAArea = triangleA.findArea();
+        System.out.println(TriangleAArea);
+
+        double TriangleBArea = triangleB.findArea();
+        System.out.println(TriangleBArea);
+
+        System.out.println(triangleA.sideLine3);
+        System.out.println(triangleB.base);
     }
 }
---
+++ src/Triangle.java
@@ -0,0 +1,14 @@
+public class Triangle {
+    double base, height, sideLine1, sideLine2, sideLine3;
+    public Triangle(double base,double height, double sideLine1, double sideLine2, double sideLine3){
+        this.base=base;
+        this.height= height;
+        this.sideLine1 = sideLine1;
+        this.sideLine2 = sideLine2;
+        this.sideLine3 = sideLine3;
+    }
+
+    public double findArea() {
+        return (this.base*this.height)/2;
+    }
+}
